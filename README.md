31892e364e823d5a0cd8dbac9481fdc35431216e
probonopd <probonopd@users.noreply.github.com>
2025-07-02 20:39:11 +0200

    Build

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
-# Porting X11Libre to FreeBSD
+# Porting X11Libre to FreeBSD  [![Build Status](https://api.cirrus-ci.com/github/probonopd/xlibre-ports.svg)](https://cirrus-ci.com/github/probonopd/xlibre-ports)
 
 An effort for porting X11libre to FreeBSD.
 
 After cloning, you can use the OVERLAY option in the /etc/make.conf to overlay
 this folder to your main tree. 
 Use a jail for testing, so that your packages won't get messed up. 
+
+## Packages
+
+[Built packages](https://api.cirrus-ci.com/v1/artifact/github/probonopd/xlibre-ports/pkg/binary/FreeBSD:14:amd64/index.html)
+
+```sh
+sudo su
+
+cat > /usr/local/etc/pkg/repos/xlibre.conf <<\EOF
+xlibre: {
+        url: "pkg+https://api.cirrus-ci.com/v1/artifact/github/probonopd/xlibre-ports/pkg/binary/${ABI}",
+        mirror_type: "srv",
+        enabled: yes,
+        priority: 100
+}
+EOF
+exit
+
+sudo pkg install xlibre-server xlibre-drivers
+```
--- x11-servers/xlibre-server/files/patch-hw_xfree86_os-support_bsd_bsd__init.c
+++ x11-servers/xlibre-server/files/patch-hw_xfree86_os-support_bsd_bsd__init.c
@@ -44,15 +44,6 @@
              }
              else {              /* xf86Info.ShareVTs */
                  close(xf86Info.consoleFd);
-@@ -303,7 +326,7 @@ xf86OpenConsole()
-     else {
-         /* serverGeneration != 1 */
- #if defined (SYSCONS_SUPPORT) || defined (PCVT_SUPPORT)
--        if (!xf86Info.ShareVTs &&
-+        if (!xf86Info.ShareVTs && xf86Info.autoVTSwitch &&
-             (xf86Info.consType == SYSCONS || xf86Info.consType == PCVT)) {
-             if (ioctl(xf86Info.consoleFd, VT_ACTIVATE, xf86Info.vtno) != 0) {
-                 xf86Msg(X_WARNING, "xf86OpenConsole: VT_ACTIVATE failed\n");
 @@ -594,6 +617,8 @@ xf86CloseConsole()
      case SYSCONS:
      case PCVT:
@@ -62,12 +53,3 @@
          if (ioctl(xf86Info.consoleFd, VT_GETMODE, &VT) != -1) {
              VT.mode = VT_AUTO;
              ioctl(xf86Info.consoleFd, VT_SETMODE, &VT); /* dflt vt handling */
-@@ -604,7 +629,7 @@ xf86CloseConsole()
-                            strerror(errno));
-         }
- #endif
--        if (initialVT != -1)
-+        if (xf86Info.autoVTSwitch && initialVT != -1)
-             ioctl(xf86Info.consoleFd, VT_ACTIVATE, initialVT);
-         break;
- #endif                          /* SYSCONS_SUPPORT || PCVT_SUPPORT */
